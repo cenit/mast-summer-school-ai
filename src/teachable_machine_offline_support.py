2c12008e3aa75a6741828124780111bafe9c3b63
Stefano Sinigardi <stesinigardi@hotmail.com>
2022-06-22 10:30:07 +0200

    add wav example

--- src/teachable_machine_offline_support.py
+++ src/teachable_machine_offline_support.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 # Imports
-from tensorflow.lite import vision
-
+from tflite_support.task import audio
 from tflite_support.task import vision
 from tflite_support.task import core
 from tflite_support.task import processor
 
+import os
+
 default_dir = os.getcwd()
 model_path = default_dir + '/soundclassifier_with_metadata.tflite'
-print(f'Data directory will be: {model_path}')
+print(f'Model in use: {model_path}')
 
 # Initialization
 base_options = core.BaseOptions(file_name=model_path)
@@ -17,9 +18,9 @@ classification_options = processor.ClassificationOptions(max_results=2)
 options = audio.AudioClassifierOptions(base_options=base_options, classification_options=classification_options)
 classifier = audio.AudioClassifier.create_from_options(options)
 
-# Alternatively, you can create an audio classifier in the following manner:
-# classifier = audio.AudioClassifier.create_from_file(model_path)
-
 # Run inference
+audio_path = default_dir + '/../res/Ensoniq-ZR-76-01-Dope-77.wav'
+print(f'Audio for test will be: {audio_path}')
 audio_file = audio.TensorAudio.create_from_wav_file(audio_path, classifier.required_input_buffer_size)
 audio_result = classifier.classify(audio_file)
+print(f'Class: {audio_result.classifications[0].categories[0].category_name}, probability: {audio_result.classifications[0].categories[0].score}')
